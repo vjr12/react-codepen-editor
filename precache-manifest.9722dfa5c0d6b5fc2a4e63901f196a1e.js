9c4b2437c55c935fb0364e540bdbf334c27b7e4c
Velson Rosario <velsonrosario@Velsons-MacBook-Pro-13.local>
2023-01-08 15:19:56 +0530

    Updates

---
+++ precache-manifest.9722dfa5c0d6b5fc2a4e63901f196a1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f1310842ecea99e477acb7b018568c",
+    "url": "/react-codepen-editor/index.html"
+  },
+  {
+    "revision": "c8353a0e6aaf6daadf53",
+    "url": "/react-codepen-editor/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "a39c9a44a22d39ac5697",
+    "url": "/react-codepen-editor/static/css/main.355f506b.chunk.css"
+  },
+  {
+    "revision": "c8353a0e6aaf6daadf53",
+    "url": "/react-codepen-editor/static/js/2.5fc1d4ee.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/react-codepen-editor/static/js/2.5fc1d4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39c9a44a22d39ac5697",
+    "url": "/react-codepen-editor/static/js/main.2292a979.chunk.js"
+  },
+  {
+    "revision": "2f5bd65de2a6ce32533b",
+    "url": "/react-codepen-editor/static/js/runtime-main.3867b7c9.js"
+  }
+]);
